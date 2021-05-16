a8efeefda87661d2e378ae069fe780ae542df967
Xoce97 <xocerodrigo@gmail.com>
2021-05-16 16:46:17 -0500

    xoce97
    Se estan creando la api y las funciones para el registro de usuarios

--- adminPassword/registroUsuario/views.py
+++ adminPassword/registroUsuario/views.py
@@ -1,4 +1,5 @@
 from os import error
+from django import template
 from django.shortcuts import render,redirect
 from django.template import Template,Context
 from registroUsuario import models
@@ -6,6 +7,7 @@ from registroUsuario import Api
 from registroUsuario.Api import generar_hash_password
 
 # Create your views here.
+
 def registroUsuario(request):
     template='registroUsuario.html'
     if request.method=='GET':
@@ -16,7 +18,36 @@ def registroUsuario(request):
         password=request.POST.get('password','').strip()
         correoE=request.POST.get('correo','').strip()
         telefono=request.POST.get('telefono','').strip()
+
+
+def registroCredencial(request):
+    template='registroCredencial.html'
+    if request.method=='GET':
+        return render(request,template)
+    if request.method=='POST':
+        pass
     
-        
-    
+def usuario(request):
+    template='usuario.html'
+    return render(request,template)
+
+def logIn(request):
+    template='login.html'
+    acceso=request.session.get('acceso',False)
+    if request.method=='GET':
+        if acceso:
+            return redirect('usuario/')
+        return render(request,template) 
+    elif request.method=='POST':
+        username=request.POST.get('username','').strip()
+        password=request.POST.get('password','').strip()
+        try:
+            password_hash=Api.generar_hash_password(password)
+            models.Usuario.objects.get(Username=username,Password=password_hash)
+            request.session['acceso']=True
+            request.session['nombre']=username
+            return redirect('usuario/')
+        except:
+            errores={'Usuario o Contraseña incorrecta'}
+            return render(request,template,{'errores':errores})
         
