bb03a0ce4bb5e83bc526793d68aad0e0bb413fac
Xoce97 <xocerodrigo@gmail.com>
2021-05-16 16:16:16 -0500

    se volvio a colocar el proyecto hice algo que no debia y no se donde jala

--- adminPassword/registroU/api.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-def hash_password(password_usuario):
-    hasher=hashlib.sha512()
-    hasher.update(password_usuario.encode('utf-8'))
-    return hasher.hexdigest()
--- adminPassword/registroU/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegistrouConfig(AppConfig):
-    name = 'registroU'
