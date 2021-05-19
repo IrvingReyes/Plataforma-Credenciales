50d7673975c11429a849a41326ca3fa30ce0ad0f
IrvingReyes <ve00386@innovaccion.mx>
2021-05-18 21:25:13 -0500

    Mejoramos el sript run.sh para que pueda validar parametros y ejecutar más acciones con manage.py.

--- adminPassword/run.sh
+++ adminPassword/run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+function modouso() {
+echo "Este es el script para inicializar el archivo manage.py con variables de entorno para la configuración sensible."
+echo "Las variables de entorno se obtienen de un archivo .env"
+echo "MODO DE USO:"
+echo "run.sh <archivo.env> <parametro-manage.py>"
+}
 
+
+[[ "$#" == 2 ]] || { echo "Numero de parametros incorrecto"; modouso; exit 1;}
 [[ -f "$1" ]] || { echo "se esperaba como primer parámetro un archivo env"; exit 1; }
+
 
 for linea in $(ccdecrypt -c "$1"); do
 	export $linea
-	echo $pwdbd
-	echo $llave
 done
 
-python3 manage.py runserver
+python3 manage.py $2
