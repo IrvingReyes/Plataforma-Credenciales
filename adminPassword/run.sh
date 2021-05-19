7e367bda20373cf9614426ae206313a60d079386
IrvingReyes <ve00386@innovaccion.mx>
2021-05-18 20:53:53 -0500

    Hicimos la práctica de manejo de configuración sensible, añadimos el script run.sh y el archivo conf.env.cpt. Cambiamos la configuración de base de datos. Se crearon de variables de entorno y se separó la información sensible de la configuración.

--- adminPassword/run.sh
+++ adminPassword/run.sh
@@ -1,9 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
 [[ -f "$1" ]] || { echo "se esperaba como primer parámetro un archivo env"; exit 1; }
 
 for linea in $(ccdecrypt -c "$1"); do
-	echo "export $linea"
+	export $linea
+	echo $pwdbd
+	echo $llave
 done
 
-python manage.py runserver
+python3 manage.py runserver
