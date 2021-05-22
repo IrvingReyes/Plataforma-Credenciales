264cca65a7b573b643f3a363f1d0b1bad1e76ac3
IrvingReyes <ve00386@innovaccion.mx>
2021-05-22 15:03:17 -0500

    Se subieron las funciones para validar intentos

--- adminPassword/registroUsuario/Api.py
+++ adminPassword/registroUsuario/Api.py
@@ -33,7 +33,48 @@ def generar_iv():
     return iv
 
 
+#Funciones para validar el numero de intentos.
+
 def generar_hash_password(password_usuario):
     hasher=hashlib.sha512()
     hasher.update(password_usuario.encode('utf-8'))
     return hasher.hexdigest()
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def diferencia_segundos_ahora(tiempo):
+    ahora = datetime.datetime.now(timezone.utc)
+    diferencia = ahora - tiempo
+    return diferencia.seconds
+
+def puede_intentar(ip):
+    """
+    """
+    #Primer caso la ip es nueva
+    registro_guardado  = models.Intentos_por_IP.objects.filter(pk=ip)
+    if not registro_guardado:
+        registro = models.Intentos_por_IP(ip=ip, contador=1, ultima_peticion=datetime.datetime.now())
+        registro.save()
+        return True
+    registro_guardado = registro_guardado[0]
+    diferencia_tiempo = diferencia_segundos_ahora(registro_guardado.ultima_peticion)
+    if diferencia_tiempo > 60:
+        registro_guardado.ultima_peticion = datetime.datetime.now()
+        registro_guardado.contador = 1
+        registro_guardado.save()
+        return True
+    else:
+        if registro_guardado.contador < 3:
+            registro_guardado.ultima_peticion = datetime.datetime.now()
+            registro_guardado.contador += 1
+            registro_guardado.save()
+            return True
+        else:
+            registro_guardado.ultima_peticion =  datetime.datetime.now()
